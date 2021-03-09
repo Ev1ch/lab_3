3a4e917d6aa7d1b7d7bef0fb3d894415b6c2ef7f
Vlad Zusko <vladzvu11@gmail.com>
2021-03-09 11:49:08 +0200

    Colored, added menu, fixed insertion and getting logic

--- main.cpp
+++ main.cpp
@@ -1,6 +1,13 @@
 #include <string>
 #include <fstream>
 #include <iostream>
+#include <stdlib.h>
+
+#define YELLOW "\033[33m"
+#define RED "\033[31m"
+#define GREEN "\033[32m"
+#define WHITE "\e[37m"
+#define NC "\e[0m"
 
 class Dictionary
 {
@@ -33,12 +40,13 @@ private:
                 wordsNumber++;
                 std::string currentWord = currentLine.substr(0, currentLine.find(';')),
                             currentDefinition = currentLine.substr(currentLine.find(';') + 2);
-                this->insertWord(currentWord, currentDefinition);
+                this->insertWord(currentWord, currentDefinition, false);
             }
+            std::cout << GREEN "Dictionary is imported." NC << std::endl;
         }
         else
         {
-            std::cout << "Something wrong with file..." << std::endl;
+            std::cout << RED "Something wrong with file..." NC << std::endl;
         }
     }
 
@@ -48,32 +56,80 @@ public:
         createDictionary(dictionaryFileName);
     }
 
-    void insertWord(std::string key, std::string value)
+    void insertWord(std::string key, std::string value, bool toPrint)
     {
         int currentHash = this->makeHash(key);
-
         if (this->dictionary[currentHash].first.empty())
         {
             this->dictionary[currentHash] = {key, value};
+            if (toPrint)
+            {
+                std::cout << GREEN "Word was inserted." NC << std::endl;
+            }
+        }
+        else if (this->dictionary[currentHash].first == key)
+        {
+            if (toPrint)
+            {
+                std::string choice = "";
+                std::cout << RED "Word already exists." << std::endl;
+                std::cout << "Do you want to replace its definition?" << std::endl;
+                std::cout << "1. Yes" << std::endl;
+                std::cout << "2. No" WHITE << std::endl;
+                std::cin >> choice;
+
+                if (choice == "1")
+                {
+                    this->dictionary[currentHash].second = value;
+                    std::cout << GREEN "Word was inserted." NC << std::endl;
+                }
+                else
+                {
+                    std::cout << RED "Word was not inserted." NC << std::endl;
+                }
+            }
+            else
+            {
+                this->dictionary[currentHash].second = value;
+            }
         }
         else
         {
             while (!this->dictionary[currentHash].first.empty())
             {
                 currentHash++;
+                if (currentHash > this->dictionarySize)
+                {
+                    std::cout << RED "Dictionary is full." NC << std::endl;
+                    return;
+                }
             }
             this->dictionary[currentHash] = {key, value};
         }
     }
 
-    std::string getDefinition(std::string key)
+    void
+    getDefinition(std::string key)
     {
         int hash = this->makeHash(key);
+        bool isFound = true;
         while (this->dictionary[hash].first != key)
         {
+            if (hash > this->dictionarySize)
+            {
+                isFound = false;
+                break;
+            }
             hash++;
         }
-        return this->dictionary[hash].second;
+        if (isFound)
+        {
+            std::cout << WHITE "- " << this->dictionary[hash].second << std::endl;
+        }
+        else
+        {
+            std::cout << RED "The definition of this word is not avalible" NC << std::endl;
+        }
     }
 
     void printDictionary()
@@ -83,10 +139,56 @@ public:
             std::cout << "[" << i + 1 << "] " << this->dictionary[i].first << " | " << this->dictionary[i].second << std::endl;
         }
     }
+
+    void menu()
+    {
+        std::string choice = "";
+        std::cout << YELLOW "What do you want to do?" << std::endl;
+        std::cout << "1. Look up for a word." << std::endl;
+        std::cout << "2. Insert your definition." << std::endl;
+        std::cout << "3. Exit." WHITE << std::endl;
+        std::cin >> choice;
+        std::cout << std::endl;
+
+        std::string word, definition;
+        if (choice == "1")
+        {
+            std::cout << YELLOW "Enter a word: " WHITE << std::endl;
+            std::cin >> word;
+            getDefinition(word);
+        }
+        else if (choice == "2")
+        {
+            std::cout << YELLOW "Enter a word: " WHITE << std::endl;
+            std::cin >> word;
+            std::cin.ignore(10000, '\n');
+            std::cout
+                << YELLOW "Enter a definition: " WHITE << std::endl;
+            std::getline(std::cin, definition);
+            this->insertWord(word, definition, true);
+        }
+        else if (choice == "3")
+        {
+            std::cout
+                << YELLOW "Godbye." NC
+                << std::endl;
+            return;
+        }
+        else
+        {
+            std::cout
+                << RED "Wrong choice. Try again." NC
+                << std::endl;
+        }
+        std::cout << std::endl;
+        this->menu();
+    }
 };
 
 int main()
 {
     Dictionary dictionary("dictionary.txt");
-    std::cout << dictionary.getDefinition("B") << std::endl;
+    dictionary.menu();
+
+    return 0;
 }
